29f246392c1e0f601dcd27cb2bdb50606dea34d2
sarowar-al-amin <sajib.sarowaralamin123@gmail.com>
2021-07-15 20:41:12 +0600

    Fixing some typo and adding back button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'recepies',
+    path: 'recipes',
     children: [
       {
         path: '',
--- src/app/recepies/recipe.service.ts
+++ src/app/recepies/recipe.service.ts
@@ -10,7 +10,7 @@ export class RecipeService {
     {
       id: 's1',
       title: 'Kala bhuna',
-      imageUrl: 'https://cookpad.com/us/recipe/images/2cca2e5dd84f3d3c',
+      imageUrl: 'https://images.app.goo.gl/ZpzDTQryyPEbDTyq6',
       ingredients: ['raw cumin 1 tsp', 'nutmeg 1/4', 'mace 2 pcs', 'cloves 6 pcs', 'black pepper 1 tsp', 'green cardamom 6 pcs']
     },
     {
